29e8876f0dbe8fd77e3d06a30feb914f44f3fa99
DylanLiesenfelt <120532193+DylanLiesenfelt@users.noreply.github.com>
2025-02-23 21:38:05 -0500

    build: projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Gallery from './components/Gallery'
+import Project from './components/Project'
 import './App.css'
 
 function App() {
@@ -44,8 +45,8 @@ function App() {
               I joined the US Navy after highschool and spent 8 years stationed in San Diego. I worked as an Aviation-Electrician's Mate on H-60 model helicopters.<br/><br/>
               In 2022 I decided to exit service receiving an Honorable discharge as an AE2 (E-5). I returned home and began using my GI Bill to to further my education.<br/><br/>
               With my trouble shooting skills from being an electrician, and my intrests in computers I decided to study Computer Science.<br/><br/>
-              In 2024 I recieved my Ascoiates from Broward College with honors, shortly after that I started attending Florida Atlantic University for bacholers majoring in Computer Science and minoring in Artifical Intelligence.<br/><br/>
-              I currently live in West Palm Beach, FL with amazing girlfriend and our 3 awesome siberian Huskies; Medusa, Leo, and Forrest.<br/><br/>
+              In 2024 I recieved my asscoiate degree from Broward College with honors. Shortly after I began attending Florida Atlantic University for my bachelor's, majoring in Computer Science, and minoring in Artifical Intelligence.<br/><br/>
+              I currently live in West Palm Beach, FL with amazing girlfriend and our three awesome Siberian Huskies; Medusa, Leo, and Forrest.<br/><br/>
               Thanks for coming to my website, have a look around!</p>
               <div className='photo-gallery'>
                 <Gallery startIndex={0} />
@@ -67,7 +68,7 @@ function App() {
           )}
           {activeTab === 'projects' && (
             <div className={`content ${activeTab === 'projects' ? 'active' : ''}`}>
-              <h3>Projects</h3>
+              <Project></Project>
             </div>
           )}
         </div>
---
+++ src/components/Project.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { useState } from "react"
+import './project.css'
+
+const Project = (name, ) => {
+
+
+    return (
+        <div className="project">
+            <h3>{name}</h3>
+        </div>
+    )
+}
+
+export default Project
