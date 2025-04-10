68a27e27ae3406bcf6da46beae1f4149504049f8
Dylan Liesenfelt <djliesenfelt@gmail.com>
2025-04-09 22:04:02 -0400

    update: resume

--- src/routes/Resume.jsx
+++ src/routes/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
         <div id='resume'>
             <h3>Resume</h3>
             <div>
-                <button onClick={() => window.open('/Dylan_Liesenfelt_Resume.pdf', '_blank')}>View on Mobile</button>
+                <button onClick={() => window.open('/Resume-Dylan-Liesenfelt.pdf', '_blank')}>View on Mobile</button>
             </div>
         </div>
     )
